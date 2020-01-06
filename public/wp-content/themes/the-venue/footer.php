8ab7097a9ada168787c5337ee6de5cf6b77a7e06
“Phong <phong@phinguin.com>
2020-01-06 23:20:16 +0700

    Begin header

--- public/wp-content/themes/the-venue/footer.php
+++ public/wp-content/themes/the-venue/footer.php
@@ -11,26 +11,12 @@
 
 ?>
 
-	</div><!-- #content -->
+	
+	<footer>
+		<p>Footer</p>
+	</footer>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'the-venue' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'the-venue' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'the-venue' ), 'the-venue', '<a href="http://Passion%20Corner">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
 
 <?php wp_footer(); ?>
-
 </body>
 </html>
